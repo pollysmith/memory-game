f5c7439f7ecbd1229b962515441cdf3cafb22d8e
Polly-Chen-Smith <pollychen07@gmail.com>
2018-08-20 11:32:07 -0700

    added modal with template stats

--- js/app.js
+++ js/app.js
@@ -187,3 +187,13 @@ function stopClock() {
 }
 /*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ function toggleModal() {
+     const modal = document.querySelector('.modal_background');
+     modal.classList.toggle('hide');
+ }
+
+ toggleModal()
+
+
+
